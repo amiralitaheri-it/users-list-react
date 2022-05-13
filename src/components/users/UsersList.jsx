2f28442a8c7f2c1c54ff322edf9fd961c906de3f
NasseriYa <webme724@gmail.com>
2022-05-13 15:30:58 +0430

    fix body scroll. add div for scroll in element

--- src/components/users/UsersList.jsx
+++ src/components/users/UsersList.jsx
@@ -7,21 +7,24 @@ function UsersList() {
     const userContext = useContext(UsersContext);
 
     return (
-        <table className="mt-10 max-w-5xl mx-auto table-auto rounded-lg">
-            <TableHeader/>
-            <tbody className="bg-gray-200 border-2 rounded-lg">
-            {
-                userContext.users.length === 0
-                    ? <tr>
-                        <td colSpan="9" className="text-center text-rose-800 p-5 text-2xl font-semibold">There aren`t any
-                            users :(
-                        </td>
-                    </tr>
-                    : userContext.users.map((user, index) => <UserItem key={index + Math.random() * 1000}
-                                                                       user={user} index={index}/>)
-            }
-            </tbody>
-        </table>
+        <div class="overflow-auto">
+            <table className="mt-10 max-w-5xl mx-auto table-auto rounded-lg">
+                <TableHeader/>
+                <tbody className="bg-gray-200 border-2 rounded-lg">
+                {
+                    userContext.users.length === 0
+                        ? <tr>
+                            <td colSpan="9" className="text-center text-rose-800 p-5 text-2xl font-semibold">There aren`t any
+                                users :(
+                            </td>
+                        </tr>
+                        : userContext.users.map((user, index) => <UserItem key={index + Math.random() * 1000}
+                                                                        user={user} index={index}/>)
+                }
+                </tbody>
+            </table>
+        </div>
+
     );
 }
 
